5a93349d65c791a03d5743271b5c14fc0db0343c
mhmd-sjad-nfsi <mhmdsjadnfysy@gmail.com>
2025-07-04 18:07:49 +0330

    Feat: Install MUI and create basic Header component

---
+++ frontend/src/components/Header.jsx
@@ -0,0 +1,26 @@
+import React from 'react';
+import { AppBar, Toolbar, Typography, Container } from '@mui/material';
+
+const Header = () => {
+  return (
+    <AppBar position="static">
+      <Container maxWidth="lg">
+        <Toolbar disableGutters>
+          <Typography
+            variant="h6"
+            component="a"
+            href="/"
+            sx={{
+              color: 'inherit',
+              textDecoration: 'none',
+            }}
+          >
+            دیجی شاپ
+          </Typography>
+        </Toolbar>
+      </Container>
+    </AppBar>
+  );
+};
+
+export default Header;
