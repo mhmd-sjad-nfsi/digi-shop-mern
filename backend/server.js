4565a1f0a0c8f94f938d6000db733c5df677335b
mhmd-sjad-nfsi <mhmdsjadnfysy@gmail.com>
2025-07-04 17:37:55 +0330

    Setup: Initial backend with Express and concurrent script

---
+++ backend/server.js
@@ -0,0 +1,13 @@
+const express = require('express');
+
+const app = express();
+
+app.get('/', (req, res) => {
+  res.send('API is running...');
+});
+
+const PORT = process.env.PORT || 5000;
+
+app.listen(PORT, () => {
+  console.log(`Server running in development mode on port ${PORT}`);
+});
