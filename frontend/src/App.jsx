f3091493172a74a4c104ee35443a43f00564188e
mhmd-sjad-nfsi <mhmdsjadnfysy@gmail.com>
2025-07-05 17:52:19 +0330

    Feat: Install and setup React Router with BrowserRouter

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,4 +1,5 @@
 import React from 'react';
+import { Routes, Route } from 'react-router-dom';
 import Header from './components/Header';
 import Footer from './components/Footer';
 import { Box, Container } from '@mui/material';
@@ -18,3 +19,4 @@ function App() {
     </Box>
   );
 }
+export default App;
--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -7,11 +7,15 @@ import { ThemeProvider } from '@mui/material/styles';
 import CssBaseline from '@mui/material/CssBaseline';
 import theme from './theme';
 
+import { BrowserRouter } from 'react-router-dom';
+
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <ThemeProvider theme={theme}>
-      <CssBaseline /> 
-      <App />
-    </ThemeProvider>
+    <BrowserRouter> 
+      <ThemeProvider theme={theme}>
+        <CssBaseline />
+        <App />
+      </ThemeProvider>
+    </BrowserRouter>
   </React.StrictMode>
 );
