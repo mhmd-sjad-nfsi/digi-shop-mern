c88cacaa7dfdcff39d244a5a62045d480d8e2f33
mhmd-sjad-nfsi <mhmdsjadnfysy@gmail.com>
2025-07-05 17:40:38 +0330

    Feat: Display products on HomePage using MUI Grid

--- frontend/src/pages/HomePage.jsx
+++ frontend/src/pages/HomePage.jsx
@@ -1,6 +1,7 @@
 import React from 'react';
-import { Container, Typography } from '@mui/material';
-// import productsData from '../productsData';
+import { Container, Grid, Typography } from '@mui/material';
+import productsData from '../productsData';
+import ProductCard from '../components/ProductCard'; 
 
 const HomePage = () => {
   return (
@@ -8,7 +9,14 @@ const HomePage = () => {
       <Typography variant="h4" component="h1" gutterBottom>
         آخرین محصولات
       </Typography>
-      {/* در جلسات بعدی، اینجا محصولات را با map کردن نمایش می‌دهیم */}
+      <Grid container spacing={3}> 
+        {productsData.map((product) => (
+         
+          <Grid item key={product._id} xs={12} sm={6} md={4} lg={3}>
+            <ProductCard product={product} />
+          </Grid>
+        ))}
+      </Grid>
     </Container>
   );
 };
