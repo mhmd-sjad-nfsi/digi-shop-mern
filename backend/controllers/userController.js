8d08cc19cc54f107224309a14df52863b2bd37dc
mhmd-sjad-nfsi <mhmdsjadnfysy@gmail.com>
2025-07-20 12:46:03 +0330

    43 PROB HEADER

--- backend/controllers/userController.js
+++ backend/controllers/userController.js
@@ -36,7 +36,12 @@ const authUser = asyncHandler(async (req, res) => {
   const user = await User.findOne({ email });
 
   if (user && (await user.matchPassword(password))) {
-    res.json({ /* ...اطلاعات کاربر و توکن... */ });
+    res.json({ 
+      _id: user._id,
+      name: user.name,
+      email: user.email,
+      isAdmin: user.isAdmin,               
+      token: generateToken(user._id), });
   } else {
     res.status(401);
     throw new Error('ایمیل یا رمز عبور نامعتبر است');
