eee42587eeba9a033843aab9d79f5c91e6149f11
mhmd-sjad-nfsi <mhmdsjadnfysy@gmail.com>
2025-10-15 18:35:05 +0330

    feat: apply MUI theme + , add Vazirmatn font and full RTL support

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -5,10 +5,27 @@ import './index.css';
 import App from './App.jsx';
 import { CssBaseline } from '@mui/material';
 
+import { ThemeProvider } from '@mui/material/styles';
+import theme from './theme.js';
+
+import createCache from '@emotion/cache';
+import { CacheProvider } from '@emotion/react';
+import rtlPlugin from 'stylis-plugin-rtl';
+import { prefixer } from 'stylis';
+
+const cacheRtl = createCache({
+  key: 'muirtl',
+  stylisPlugins: [prefixer, rtlPlugin],
+});
+
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
+    <CacheProvider value={cacheRtl}>
+      <ThemeProvider theme={theme}>
     <CssBaseline/>
     <App />
+      </ThemeProvider>
+    </CacheProvider>
   </StrictMode>,
 )
